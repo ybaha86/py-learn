3b5893d5956636624dafa74814cd7c394339a6fc
Yunus <yunus@Yunuss-MacBook-Air.local>
2023-12-18 06:42:08 +0700

    switch value variable

--- basic.py
+++ basic.py
@@ -1,2 +1,28 @@
 #Calculate Lenght of String 
-print(len(input()))
+# print(len(input()))
+
+#switch value between two variable
+a = input()
+b = input()
+
+# a = 10
+# b = 99
+# c = 99
+# a = 99
+# 
+
+# Switch value 1
+# c = b
+# d = a
+# a = c 
+# b = d
+
+# switch value 2
+c = a
+a = b 
+b = c 
+
+
+# Print value
+print("value a " + a)
+print("value b " + b)
