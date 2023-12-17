7aea94ffd0bcdfc99d6f7fb9a0407523505e3467
Yunus <yunus@Yunuss-MacBook-Air.local>
2023-12-18 06:52:30 +0700

    first project

---
+++ project1.py
@@ -0,0 +1,4 @@
+print ("Welcome Band Name Generator")
+name_city = input ("what's name of your city your grew up\n")
+name_pet = input ("What's your pet name\n")
+print ("Your band name could be " + name_city + " " + name_pet )
