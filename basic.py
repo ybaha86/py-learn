afe169b20c2672a85b44e45bb57a1ce92e4cc81f
Yunus <yunus.bahar86@gmail.com>
2023-12-24 07:52:27 +0700

    via imac

--- basic.py
+++ basic.py
@@ -1,28 +1,36 @@
-#Calculate Lenght of String 
-# print(len(input()))
+# #Calculate Lenght of String 
+# # print(len(input()))
 
-#switch value between two variable
-a = input()
-b = input()
+# #switch value between two variable
+# a = input()
+# b = input()
 
-# a = 10
-# b = 99
-# c = 99
-# a = 99
-# 
+# # a = 10
+# # b = 99
+# # c = 99
+# # a = 99
+# # 
 
-# Switch value 1
-# c = b
-# d = a
-# a = c 
-# b = d
+# # Switch value 1
+# # c = b
+# # d = a
+# # a = c 
+# # b = d
 
-# switch value 2
-c = a
-a = b 
-b = c 
+# # switch value 2
+# c = a
+# a = b 
+# b = c 
 
 
-# Print value
-print("value a " + a)
-print("value b " + b)
+# # Print value
+# print("value a " + a)
+# print("value b " + b)
+
+# print("hello world")
+
+import random
+
+rand_dec = random.random()
+random_int = random.randint(0,rand_dec)
+print(random_int)
